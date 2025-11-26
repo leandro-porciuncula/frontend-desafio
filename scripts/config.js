c5b187be2b3d1bcaa9eb07c119d5c320974fa16b
leandroP <leandro@cloudtreinamentos.com>
2025-11-26 18:12:16 -0300

    pc

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
 // Detecta o ambiente automaticamente (localhost ou produção)
-const BACKEND_URL = 'http://ENDEREÇO_DO_BACKEND:3000'; // Altere aqui para o IP da sua EC2
+const BACKEND_URL = 'https://ENDEREÇO_DO_BACKEND:3000'; // Altere aqui para o IP da sua EC2
