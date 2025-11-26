d477122fb205563bbeb00599f2134314799e1838
leandro-porciuncula <64909980+leandro-porciuncula@users.noreply.github.com>
2025-11-26 15:47:48 -0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,5 @@
 // Detecta o ambiente automaticamente (localhost ou produção)
 //const BACKEND_URL = 'http://ENDEREÇO_DO_BACKEND:3000'; // Altere aqui para o IP da sua EC2
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
-
-export default {
-  BACKEND_URL
+window.CONFIG = {
+  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:3000"
 };
-
