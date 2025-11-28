2fbd9c138f28510d1d925a97a77b226fdfadd2f3
leandro-porciuncula <64909980+leandro-porciuncula@users.noreply.github.com>
2025-11-27 21:48:36 -0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
 // Detecta o ambiente automaticamente (localhost ou produção)
-const BACKEND_URL = 'https://testebackdesafio.labs.upperstack.com.br:3000'; // Altere aqui para o IP da sua EC2
+const BACKEND_URL = 'https://backend-desafio.labs.upperstack.com.br:3000'; // Altere aqui para o IP da sua EC2
